9614920aede03d595a2436d86db326264ef24eb0
John Blake <john.blake@verizon.net>
2023-08-02 18:34:15 -0400

    Add loop with an option to run a search for a different party.  Eliminate unnecessary f string

--- eluciDoc.py
+++ eluciDoc.py
@@ -68,60 +68,66 @@ nlp = spacy.load("en_core_web_lg")
 doc = nlp(text)
 sentences: list[str] = [sentence.text for sentence in doc.sents]
 
-master_List = []
-search_phrase_list: list[str] = []
-
-party: str = input('Enter the term for the party to be searched (entry is case sensitive if that option selected:')
-if case_sensitive == 'No':
-    party = party.lower()
-concord(party, text)
-print('\n', 'Run Excel?')
-excel_Response = pyip.inputMenu(['Yes', 'No'], numbered=True)
-if excel_Response == 'Yes':
-    subprocess.Popen(r'C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE')
-
 while True:
-    secTerm: str = input(r'''Enter a predicate search term or phrase - include "'s" or "'" for possessive case of the
-     party being searched:''')
+    master_List = []
+    search_phrase_list: list[str] = []
 
-    if not secTerm.startswith("'"):  # add a space where the second term is not a possessive
-        secTerm = " ".join(["", secTerm])
-
-    search_phrase = f'{party}' + secTerm
+    party: str = input('Enter the term for the party to be searched (entry is case sensitive if that option selected:')
     if case_sensitive == 'No':
-        search_phrase = search_phrase.lower()
+        party = party.lower()
+    concord(party, text)
+    print('\n', 'Run Excel?')
+    excel_Response = pyip.inputMenu(['Yes', 'No'], numbered=True)
+    if excel_Response == 'Yes':
+        subprocess.Popen(r'C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE')
 
-    search_phrase_list.append(search_phrase)
-    list_Status = ' | '.join(search_phrase_list)
-    print('')
-    print('Search Phrases:', list_Status)  # track Search Phrases as entered
-    print('')
+    while True:
+        secTerm: str = input(r'''Enter a predicate search term or phrase - include "'s" or "'" for possessive case of
+         the party being searched:''')
 
-    response = pyip.inputMenu(['Enter another search term?', 'Finished'], numbered=True)
+        if not secTerm.startswith("'"):  # add a space where the second term is not an apostrophe
+            secTerm = " ".join(["", secTerm])
 
+        search_phrase = party + secTerm
+        if case_sensitive == 'No':
+            search_phrase = search_phrase.lower()
+
+        search_phrase_list.append(search_phrase)
+        list_Status = ' | '.join(search_phrase_list)
+        print('')
+        print('Search Phrases:', list_Status)  # track Search Phrases as entered
+        print('')
+
+        response = pyip.inputMenu(['Enter another search term', 'Finished for this party'], numbered=True)
+
+        if response == 'Finished for this party':
+            for sent in sentences:
+                for i in search_phrase_list:
+                    if i in sent:
+                        if sent not in master_List:
+                            master_List.append(sent)
+                continue
+            break
+
+    document = Document()
+    style = document.styles['Normal']
+    font = style.font
+    font.name = "Times New Roman"
+    font.size = Pt(12)
+    sections = document.sections
+    section = sections[0]
+    section.left_margin = Inches(1.0)
+    section.right_margin = Inches(1.0)
+
+    document.add_paragraph(f'Phrases searched: {list_Status}')
+    for j in master_List:
+        document.add_paragraph(j)
+    document.save(rf'{target_File_path}\{result_Filename}_{party}_search_result.docx')
+    subprocess.Popen([r'C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE',
+                      rf'{target_File_path}\{result_Filename}_{party}_search_result.docx'])
+    response = pyip.inputMenu(['Search another party', 'Finished'], numbered=True)
+    if response == 'Search another party':
+        continue
     if response == 'Finished':
-        for sent in sentences:
-            for i in search_phrase_list:
-                if i in sent:
-                    if sent not in master_List:
-                        master_List.append(sent)
-            continue
-        break
-
-document = Document()
-style = document.styles['Normal']
-font = style.font
-font.name = "Times New Roman"
-font.size = Pt(12)
-sections = document.sections
-section = sections[0]
-section.left_margin = Inches(1.0)
-section.right_margin = Inches(1.0)
-
-document.add_paragraph(f'Phrases searched: {list_Status}')
-for j in master_List:
-    document.add_paragraph(j)
-document.save(rf'{target_File_path}\{result_Filename}_{party}_search_result.docx')
-print("Run Finished")
-subprocess.Popen([r'C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE',
-                  rf'{target_File_path}\{result_Filename}_{party}_search_result.docx'])
+        print('Run Finished')
+    break
